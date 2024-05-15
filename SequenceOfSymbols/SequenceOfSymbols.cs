7835880e0c64acafeb2ee58f97157f1d80a0ea8b
Diana Kosel <dianakosel@gmail.com>
2024-05-15 12:06:29 +0200

    add:SequenceOfSymbols.cs, Main and GetNumberOfSymbols method to the SequenceOfSymbols class

---
+++ SequenceOfSymbols/SequenceOfSymbols.cs
@@ -0,0 +1,39 @@
+﻿class SequenceOfSymbols
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("No sequence of symbols provided.");
+            return;
+        }
+
+        foreach (string symbols in args)
+        {
+            int number = GetNumberOfSymbols(symbols);
+            Console.WriteLine($"Maximum number of unequal consecutive characters in '{symbols}' per line is {number}");
+        }
+    }
+
+    static int GetNumberOfSymbols(string symbols)
+    {
+        if (string.IsNullOrEmpty(symbols))
+            return 0;
+
+        int number = 0;
+        int currentNumber = 0;
+
+        for (int i = 1; i < symbols.Length; i++)
+        {
+            if (symbols[i] != symbols[i - 1])
+                currentNumber++;
+            else
+            {
+                number = Math.Max(number, currentNumber);
+                currentNumber = 0;
+            }
+        }
+
+        return Math.Max(number, currentNumber);
+    }
+}
